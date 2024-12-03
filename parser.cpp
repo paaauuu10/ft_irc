5853b15a08d22b05b277a360849c3c851cd64811
Pau Botargues Julio <pbotargu@student.42barcelona.com>
2024-12-03 09:29:13 +0100

    nothing

--- parser.cpp
+++ parser.cpp
@@ -1,18 +1,86 @@
-/* ************************************************************************** */
+ /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   parser.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pbotargu <pbotargu@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pbotargu <pbotargu@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 10:31:53 by pbotargu          #+#    #+#             */
-/*   Updated: 2024/11/25 12:56:32 by pbotargu         ###   ########.fr       */
+/*   Updated: 2024/11/29 11:27:35 by pbotargu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <algorithm>
 #include <cctype> 
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Client.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anovio-c <anovio-c@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 11:31:40 by anovio-c          #+#    #+#             */
+/*   Updated: 2024/11/25 17:17:54 by anovio-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+class Client {
+	private:
+		int							 _fd;
+		std::string					_username;
+		std::string 				_nickname; // nickname max 9 chars
+		// See the protocol grammar rules for what may and may not be used in a nickname. 
+		bool						_logged;
+		bool						_isRegistered;
+		std::string					_buffer;
+		std::vector<std::string>	_channels;
+		
+	public:
+		Client();
+		Client(std::string username, std::string nickname, int fd);
+		Client(const Client &src);
+		Client &operator=(const Client &src);
+		~Client();
+
+		// Getters
+
+		int			getFd(void);
+		std::string	getUsername(void);
+		std::string	getNickname(void);
+		bool		getLogged(void);
+		std::string	getBuffer(void);
+		bool		getRegistered(void);
+		bool		getChannel(std::string &channelName);
+		// Setters
+
+		bool	setNickname(std::string &nickname);
+		bool	setUsername(std::string &username);
+		void	setFd(int fd);
+		void	setLogged(bool value);
+		void	setBuffer(std::string &buffer);
+		void	setRegistered(bool value);
+		
+		// Member functions
+		void	addChannel(std::string &channelName);
+		void	rmChannel(std::string &channelName);
+		void	freeBuffer(void);
+};
+
+
+void pass(std::string pass, Client client)
+{ 
+    if (client.getRegistered())
+    {
+        std::cout << "Already registered" << std::endl;
+    }
+    if (pass != _pass) /*Modificar "HOLA" por la variable donde almacenemos la contraseña*/)
+
+}
+
 
 
 bool validCommand(std::string str)
@@ -100,5 +168,7 @@ void parser(std::string str)
 int main(int argc, char **argv)
 {
     (void)argc;
+    Client *client;
     parser(argv[1]);
+    pass(argv[2], *client);
 }
