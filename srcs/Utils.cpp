a4d6e8a725f6236de3b72cbeeede1c7a6d1072c3
Pau Borrull Martínez <pborrull@car10s4.42barcelona.com>
2024-12-05 11:56:24 +0100

    NICK almost finished

---
+++ srcs/Utils.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Utils.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anovio-c <anovio-c@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/03 12:00:21 by anovio-c          #+#    #+#             */
+/*   Updated: 2024/12/03 12:00:22 by anovio-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Utils.hpp"
+
+// :<server> <error_code> <target> :<error_message>
+// :irc.example.com 432 user123 :Erroneous nickname
+
+// target se predefine en "", si no es pasado como parametro
+void sendError(Client* client, int errorCode, const std::string& errorMessage, const std::string& target = "") {
+    if (!client || !client->getLogged()) {
+        std::cerr << "Error: Cliente no está conectado.\n";
+        return;
+    }
+
+    std::string serverName = Server::getServerName();
+
+    std::ostringstream oss;
+    oss << ":" << serverName << " " << errorCode << " " 
+        << (target.empty() ? "*" : target) // Usa "*" si el target no está definido.
+        << " :" << errorMessage;
+    //client->SendMessage(oss.str());
+}
