c2c790043e02267c92d5cbd40bab6af27f9f8ae1
paaauuu10 <pau.botargues@gmail.com>
2024-12-12 11:55:52 +0100

    <PASS>: now If + else if

--- srcs/cmds/PASS.cpp
+++ srcs/cmds/PASS.cpp
@@ -6,7 +6,7 @@
 /*   By: pbotargu <pbotargu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 10:57:43 by pbotargu          #+#    #+#             */
-/*   Updated: 2024/12/12 11:40:48 by pbotargu         ###   ########.fr       */
+/*   Updated: 2024/12/12 11:54:38 by pbotargu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,23 @@
 
 void	PASS( Client *client, std::string pass)
 {
-   	std::string password = Server::getInstance().getPass(); // Conseguimos PASS del sever
-    std::string clean_pass = pass.substr(pass.find(' ') + 1); // Eliminar el salto de línea (si existe) al final
+   	std::string password = Server::getInstance().getPass(); // We get PASS from Server
+    std::string clean_pass = pass.substr(pass.find(' ') + 1); // Delete initial space
     while (!clean_pass.empty() && ((clean_pass[clean_pass.size() - 1]) == '\n' || (clean_pass[clean_pass.size() - 1]) == '\r'))
     {
     	clean_pass.erase(clean_pass.size() -1);
     }
    
 	if (pass.empty())
-    {
         sendError(client, 461, "Not enough parameters", pass); //ERR_NEEDMOREPARAMS
-        return ;
-    }
-    if (client->getLogged() == true)
-    {
+    else if (client->getLogged() == true)
 	    sendError(client, 462, "Unauthorized command (already registered)"); //ERR_ALREADYREGISTRED
-        return ;
-    }
-
-    if (clean_pass != password)
-    {
+    else if (clean_pass != password)
         sendError(client, 464, "Password incorrect"); //ERR_PASSWDMISMATCH
-        return ; 
+	else if (clean_pass == password)
+    {
+        std::string response = "Correct Password! Welcome to ft_irc!\r\n";
+        send(client->getFd(), response.c_str(), response.size(), 0);
+        client->setLogged(true);
     }
-	std::string response = "Correct Password! Welcome to ft_irc!\r\n";
-    send(client->getFd(), response.c_str(), response.size(), 0);
-    client->setLogged(true);
 }
