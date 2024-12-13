1ac20ab28f122a348248ff80e9427272403a8edd
paaauuu10 <pau.botargues@gmail.com>
2024-12-13 15:05:03 +0100

    MODE

--- srcs/cmds/MODE.cpp
+++ srcs/cmds/MODE.cpp
@@ -6,7 +6,7 @@
 /*   By: pbotargu <pbotargu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 12:22:51 by pbotargu          #+#    #+#             */
-/*   Updated: 2024/12/13 13:04:57 by pbotargu         ###   ########.fr       */
+/*   Updated: 2024/12/13 14:59:57 by pbotargu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,32 +17,77 @@
 
 void	MODE(Client *client, std::string str)
 {
-    // i t k o l cmds que s'han de controlar
+    //S'ha de mirar si es un operador abans de continuar. Si no return ; i ciao! NO borrar comentari!
     std::vector<std::string> words = split(str, ' ');
-    if (words.size() < 1)
+    //Channel *channel = Server::getInstance().getCheckChannel(words[0]);
+    if (words.size() < 2)
     {
         sendError(client, 461, "Not enough parameters\n"); //ERR_NEEDMOREPARAMS
 		return ;
     }
-    
-    for(long unsigned int i = 0; i < words[0].size() - 1; i++)
+    char s = '+';
+    for(long unsigned int i = 0; i < words[1].size(); i++)
     {
-        if (words[0][i] != '+' && words[0][i] != '-')
+        while (words[1][i] == '+' || words[1][i] == '-')
+            s = words[1][i++];      
+        if (words[1][i] == 'i' || words[1][i] == 't' || words[1][i] == 'k' || words[1][i] == 'o' || words[1][i] == 'l')
         {
-            sendError(client, 472 , ":is unknown mode char to me", words[0]); //ERR_UNKNOWNMODE    
+            std::string response;
+            if (s == '-')
+            {
+                response = "this is -" + std::string(1, words[1][i]) + "\n";
+                //channel->setMode(words[1][i], false, 0);
+            }
+            else 
+            {
+                response = "this is +" + std::string(1,words[1][i]) + "\n";
+                //channel->setMode(words[1][i], true, 0);
+            }
+            send(client->getFd(), response.c_str(), response.size(), 0);
+        }
+       /* else if (words[1][i] == 't')
+        {
+            std::string response;
+            if (s == '-')
+                response = "this is -t\n";
+            else 
+                response = "this is mode +t!\n";
+            send(client->getFd(), response.c_str(), response.size(), 0);
+        }
+        else if (words[1][i] == 'k')
+        {
+            std::string response;
+            if (s == '-')
+                response = "this is -k\n";
+            else 
+                response = "this is mode +k!\n";
+            send(client->getFd(), response.c_str(), response.size(), 0);
+        }
+        else if (words[1][i] == 'o')
+        {
+            std::string response;
+            if (s == '-')
+                response = "this is -o\n";
+            else 
+                response = "this is mode +o!\n";
+            send(client->getFd(), response.c_str(), response.size(), 0);
+        }
+        else if (words[1][i] == 'l')
+        {
+            std::string response;
+            if (s == '-')
+                response = "this is -l\n";
+            else 
+                response = "this is mode +l!\n";
+            send(client->getFd(), response.c_str(), response.size(), 0);
+        }*/
+        else 
+        {
+            std::string charError = std::string(1, words[1][i]);
+            sendError(client, 472 , "is unknown mode char to me", charError); //ERR_UNKNOWNMODE    
             return ;
         }
     }
 
-    if (words[0][words[0].size() - 1] == 'i')
-    {
-        std::string response;
-        if (words[0][words[0].size() - 2]  && (words[0][words[0].size() - 2] == '+'))
-            response = "this is +i\n";
-        else if (words[0][words[0].size() - 2]  && (words[0][words[0].size() - 2] == '-'))
-            response = "this is -i\n";
-        else 
-            response = "this is mode i!\n";
-        send(client->getFd(), response.c_str(), response.size(), 0);
-    }
+   
 }
