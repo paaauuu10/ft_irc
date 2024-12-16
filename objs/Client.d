503a2f19efcc0009f4bd444282a6ffdf119e45a1
Pau Borrull Martínez <pborrull@car10s4.42barcelona.com>
2024-12-16 12:11:49 +0100

    PRIVMSG between clients

--- objs/Client.d
+++
@@ -1,3 +0,0 @@
-objs/Client.o: srcs/Client.cpp inc/Client.hpp inc/Channel.hpp
-inc/Client.hpp:
-inc/Channel.hpp:
--- objs/cmds/PASS.d
+++
@@ -1,6 +0,0 @@
-objs/cmds/PASS.o: srcs/cmds/PASS.cpp inc/Server.hpp inc/Utils.hpp \
- inc/Server.hpp inc/Client.hpp
-inc/Server.hpp:
-inc/Utils.hpp:
-inc/Server.hpp:
-inc/Client.hpp:
